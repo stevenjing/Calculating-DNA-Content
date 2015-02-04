64cbdf620de79ea36e4bae68749782df696bbdc1
Steven Jing <steven.jing@gmail.com>
2015-02-04 01:23:41 +0000

    02/03 Tuesday Participation

---
+++ tuesday.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python
+from __future__ import division
+
+print "Hello World"
+
+def bradysRevenge(bro):
+	bigbro = bro.upper()
+	print "You mad, " + bigbro + "?"
+
+
+def get_at_content(dna):
+    length = len(dna)
+    a_count = dna.count('A')
+    t_count = dna.count('T')
+    at_content = (a_count + t_count) / length
+    return at_content
+
